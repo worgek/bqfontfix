c2f798dd5f991bc3af79100f55d75d6de9e15fae
worgek <orgekwilliam@gmail.com>
2025-12-02 04:07:06 +0000

    attempt 6

--- build/sources/main/java/bqfontfix/BQFontFixCoremod.java
+++ build/sources/main/java/bqfontfix/BQFontFixCoremod.java
@@ -1,38 +1,16 @@
 package bqfontfix;
 
-import java.util.Map;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import java.util.Map;
 
-@IFMLLoadingPlugin.Name("BQFontFix")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
-@IFMLLoadingPlugin.TransformerExclusions({"bqfontfix.core"})
 public class BQFontFixCoremod implements IFMLLoadingPlugin {
-
-    @Override
-        public String[] getASMTransformerClass() {
-                return new String[] {
-                            "bqfontfix.BQFontFixTransformer"
-                                    };
-                                        }
-
-                                            @Override
-                                                public String getModContainerClass() {
-                                                        return null;
-                                                            }
-
-                                                                @Override
-                                                                    public String getSetupClass() {
-                                                                            return null;
-                                                                                }
-
-                                                                                    @Override
-                                                                                        public void injectData(Map<String, Object> data) {
-                                                                                                // Not needed
-                                                                                                    }
-
-                                                                                                        @Override
-                                                                                                            public String getAccessTransformerClass() {
-                                                                                                                    return null;
-                                                                                                                        }
-                                                                                                                        }
-                                                                                                                        
+    @Override public String[] getASMTransformerClass() {
+            return new String[]{ "bqfontfix.BQFontFixFontRendererTransformer" };
+                }
+                    @Override public String getModContainerClass() { return null; }
+                        @Override public String getSetupClass() { return null; }
+                            @Override public void injectData(Map<String, Object> data) {}
+                                @Override public String getAccessTransformerClass() { return null; }
+                                }
+                                
